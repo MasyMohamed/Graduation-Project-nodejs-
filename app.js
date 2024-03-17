264e660e04546089c4c62f9c28b1e952c138ad8a
MuhammedMagdyy <mohamedmody04@gmail.com>
2024-03-17 22:00:37 +0200

    modify app.js

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const app = express();
-const cors = require("cors");
-const httpStatusText = require("./utils/httpStatusText");
-const productRouter = require("./routes/productRoute");
-const AppError = require("./utils/AppError");
-const asyncWrapper = require("./middleware/ValidationSchema");
+const cors = require('cors');
+const productRouter = require('./routes/productRoute');
+const globalErrorHandler = require('./middleware/errorMiddleware');
+const morgan = require('morgan');
+const AppError = require('./utils/AppError');
 const port = 4000;
+
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/products", productRouter);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+  console.log(`Mode: ${process.env.NODE_ENV}`);
+}
 
-// Global error handler
-app.use((error, req, res, next) => {
-  const err =
-    error instanceof AppError
-      ? error
-      : new AppError(error.message || "Internal Server Error", 500);
-  res.status(err.statusCode).json({
-    status: httpStatusText[err.statusCode] || "Internal Server Error",
-    message: err.message,
-    code: err.statusCode,
-    data: null,
-  });
+app.use('/api/products', productRouter);
+app.use('*', (req, _res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
+app.use(globalErrorHandler);
 
 const PORT = process.env.PORT || port; // Change the port number to 4001 or any other available port
 app.listen(PORT, () => {
