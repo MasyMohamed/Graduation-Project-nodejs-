da552c2dc53c59272d1a1953442e36de345b34a2
MasyMohamed <masym32@gmail.com>
2024-03-17 03:46:05 +0200

    update

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 module.exports = {
   database: {
     dialect: "postgres",
-    databaseName: process.env.DB_NAME || "BeautyMate Application",
-    username: process.env.DB_USER || "postgres",
-    password: process.env.DB_PASSWORD || "postgres",
-    host: process.env.DB_HOST || "localhost",
-    port: process.env.DB_PORT || 5432,
+    databaseName: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD ,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT ,
     dialectOptions: {
     ssl: {
       require: true, // Set to true to require SSL/TLS
