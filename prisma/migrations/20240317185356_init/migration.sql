a9eb1ce7014077e0086511f8f390fe84c0760e41
MuhammedMagdyy <mohamedmody04@gmail.com>
2024-03-17 21:47:29 +0200

    prisma config

---
+++ prisma/migrations/20240317185356_init/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "price" DECIMAL(65,30) NOT NULL,
+    "brand" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "product_image_url" TEXT NOT NULL,
+    "descrption" TEXT NOT NULL,
+    "skin_type" TEXT NOT NULL,
+    "stock_quantity" INTEGER NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
